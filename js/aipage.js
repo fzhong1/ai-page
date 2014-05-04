263cf24364d78f0bc872c05d1f4fb4760c1c0f05
Feifei Zhong <fzhong1988@gmail.com>
2014-05-04 11:22:07 -0400

    popup

--- js/aipage.js
+++ js/aipage.js
@@ -1,3 +1,28 @@
 window.onload = function(){
-	console.log("here");
+	//open popup
+	$("#requestDemo").click(function(){
+	$("#popBox").fadeIn(1000);
+	positionPopup();
+	$(".blocker").css("background-color","black");
+	});
+	 
+	//close popup
+	$("#close").click(function(){
+	$("#innerForm").fadeOut(500);
+	});
+		 
+	//position the popup at the center of the page
+	function positionPopup(){
+	if(!$("#innerForm").is(':visible')){
+	return;
+	}
+	$("#innerForm").css({
+	left: ($(window).width() - $('#innerForm').width()) / 2,
+	top: ($(window).width() - $('#innerForm').width()) / 7,
+	position:'absolute'
+	});
+	}
+	 
+	//maintain the popup at center of the page when browser resized
+	$(window).bind('resize',positionPopup);
 };
